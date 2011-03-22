c7898fa9006d6eb897b0545ba8c022f2a3477573
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-03-22 12:18:36 +0100

    Size can in theory be just as big as IPv6 :)

--- config/schema/ipv6_ranges.sql
+++ config/schema/ipv6_ranges.sql
@@ -2,9 +2,9 @@
 CREATE TABLE `ipv6_ranges` (
     `id` INT UNSIGNED NOT NULL auto_increment,
     `customer_id` INT UNSIGNED NOT NULL,
-    `address` DECIMAL( 39, 0 ) NOT NULL,
+    `address` DECIMAL( 39, 0 ) UNSIGNED NOT NULL,
     `bits` TINYINT UNSIGNED NOT NULL DEFAULT '128',
-    `size` INT UNSIGNED NOT NULL DEFAULT '0',
+    `size` DECIMAL( 39, 0 ) UNSIGNED NOT NULL DEFAULT '0',
     `created` DATETIME NOT NULL,
     `modified` DATETIME NOT NULL,
     PRIMARY KEY  (`id`)
