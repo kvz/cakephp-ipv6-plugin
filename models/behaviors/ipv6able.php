b3f29aaa918ce735cdb981c93de68f3f412a8505
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-03-24 17:24:59 +0100

    Refactoring

--- models/behaviors/ipv6able.php
+++ models/behaviors/ipv6able.php
@@ -14,8 +14,6 @@ class Ipv6ableBehavior extends ModelBehavior {
         '/isWithin/' => 'isWithin',
     );
 
-    public $settings = array();
-    
     protected $_default = array(
         'error_handler' => 'php',
         'field_address' => 'address',
@@ -23,14 +21,8 @@ class Ipv6ableBehavior extends ModelBehavior {
         'field_size' => 'size',
     );
 
-    public function setup ($Model, $settings = array()) {
-        $this->settings[$Model->alias] = array_merge(
-            $this->_default,
-            $settings
-        );
-        
-        $this->rangeByIp($Model, '2001:990:0:1522::2');
-    }
+    public $settings = array();
+
 
     public function rangeByIp ($Model, $denormalized) {
         $ip_address = $this->normalize_ipv6($denormalized);
@@ -43,79 +35,6 @@ class Ipv6ableBehavior extends ModelBehavior {
         ));
 
         return $res;
-    }
-
-    public function err ($Model, $format, $arg1 = null, $arg2 = null, $arg3 = null) {
-        $arguments = func_get_args();
-        $Model     = array_shift($arguments);
-        $format    = array_shift($arguments);
-
-        $str = $format;
-        if (count($arguments)) {
-            foreach($arguments as $k => $v) {
-                $arguments[$k] = $this->sensible($v);
-            }
-            $str = vsprintf($str, $arguments);
-        }
-
-        $this->error = $str;
-        $Model->onError();
-
-        if ($this->config['error_handler'] === 'php') {
-            trigger_error($str, E_USER_ERROR);
-        }
-
-        return false;
-    }
-
-    public function sensible ($arguments) {
-        if (is_object($arguments)) {
-            return get_class($arguments);
-        }
-        if (!is_array($arguments)) {
-            if (!is_numeric($arguments) && !is_bool($arguments)) {
-                $arguments = "'" . $arguments . "'";
-            }
-            return $arguments;
-        }
-        $arr = array();
-        foreach ($arguments as $key => $val) {
-            if (is_array($val)) {
-                $val = json_encode($val);
-            } elseif (!is_numeric($val) && !is_bool($val)) {
-                $val = "'" . $val . "'";
-            }
-
-            if (strlen($val) > 33) {
-                $val = substr($val, 0, 30) . '...';
-            }
-
-            $arr[] = $key . ': ' . $val;
-        }
-        return join(', ', $arr);
-    }
-
-    public function opt ($Model) {
-        $args  = func_get_args();
-        $count = count($args);
-        
-        if ($count > 2) {
-            $this->settings[$Model->alias][$args[1]] = $args[2];
-        } else if ($count > 1) {
-            if (!array_key_exists($args[1], $this->settings[$Model->alias])) {
-                return $this->err(
-                    $Model,
-                    'Option %s was not set',
-                    $args[1]
-                );
-            }
-            return $this->settings[$Model->alias][$args[1]];
-        } else {
-            return $this->err(
-                $Model,
-                'Opt needs more arguments (1 for Model; 1 more for getting, 2 more for setting)'
-            );
-        }
     }
 
     protected function _changeSet ($Model, $save, &$set) {
@@ -142,7 +61,7 @@ class Ipv6ableBehavior extends ModelBehavior {
         $this->_changeSet($Model, true, $Model->data[$Model->alias]);
         return true;
     }
-    
+
     public function afterFind ($Model, $results, $primary) {
         foreach ($results as $i => $result) {
             $this->_changeSet($Model, false, $results[$i][$Model->alias]);
@@ -156,7 +75,7 @@ class Ipv6ableBehavior extends ModelBehavior {
      * http://stackoverflow.com/questions/1120371/how-to-convert-ipv6-from-binary-for-storage-in-mysql/1271123#1271123
      *
      * @param string $ip_address An IP address in IPv4, IPv6 or decimal notation
-     * 
+     *
      * @return string The IP address in decimal notation
      */
     public function inet_ptod ($ip_address) {
@@ -197,7 +116,7 @@ class Ipv6ableBehavior extends ModelBehavior {
      * http://stackoverflow.com/questions/1120371/how-to-convert-ipv6-from-binary-for-storage-in-mysql/1271123#1271123
      *
      * @param string $decimal An IP address in IPv4, IPv6 or decimal notation
-     * 
+     *
      * @return string The IP address in presentation format
      */
     public function inet_dtop ($decimal) {
@@ -266,5 +185,106 @@ class Ipv6ableBehavior extends ModelBehavior {
         }
 
         return join(':', $ip);
+    }
+
+
+    public function setup ($Model, $settings = array()) {
+        $this->settings[$Model->alias] = Set::merge(
+            $this->_default,
+            $settings
+        );
+    }
+
+    public function err ($Model, $format, $arg1 = null, $arg2 = null, $arg3 = null) {
+        $arguments = func_get_args();
+        $Model     = array_shift($arguments);
+        $format    = array_shift($arguments);
+
+        $str = $format;
+        if (count($arguments)) {
+            foreach($arguments as $k => $v) {
+                $arguments[$k] = $this->sensible($v);
+            }
+            $str = vsprintf($str, $arguments);
+        }
+
+        $this->error = $str;
+        #$Model->onError();
+
+        if (@$this->settings[$Model->alias]['error_handler'] === 'php') {
+            trigger_error($str, E_USER_ERROR);
+        }
+
+        return false;
+    }
+
+    public function sensible ($arguments) {
+        if (is_object($arguments)) {
+            return get_class($arguments);
+        }
+        if (!is_array($arguments)) {
+            if (!is_numeric($arguments) && !is_bool($arguments)) {
+                $arguments = "'" . $arguments . "'";
+            }
+            return $arguments;
+        }
+        $arr = array();
+        foreach ($arguments as $key => $val) {
+            if (is_array($val)) {
+                $val = json_encode($val);
+            } elseif (is_object($val)) {
+                $val = get_class($val);
+            } elseif (!is_numeric($val) && !is_bool($val)) {
+                $val = "'" . $val . "'";
+            }
+
+            if (strlen($val) > 33) {
+                $val = substr($val, 0, 30) . '...';
+            }
+
+            $arr[] = $key . ': ' . $val;
+        }
+        return join(', ', $arr);
+    }
+
+    public function opt () {
+        $args  = func_get_args();
+
+        // Strip model from args if needed
+        if (is_object($args[0])) {
+            $Model = array_shift($args);
+        } else {
+            return $this->err('First argument needs to be a model');
+        }
+
+        // Strip method from args if needed (e.g. when called via $Model->mappedMethod())
+        if (is_string($args[0])) {
+            foreach ($this->mapMethods as $pattern => $meth) {
+                if (preg_match($pattern, $args[0])) {
+                    $method = array_shift($args);
+                    break;
+                }
+            }
+        }
+
+        $count = count($args);
+        if ($count > 1) {
+            $this->settings[$Model->alias][$args[0]] = $args[1];
+        } else if ($count > 0) {
+            if (!array_key_exists($args[0], $this->settings[$Model->alias])) {
+                return $this->err(
+                    $Model,
+                    'Option %s was not set',
+                    $args[0]
+                );
+            }
+            return $this->settings[$Model->alias][$args[0]];
+        } else {
+            return $this->err(
+                $Model,
+                'Found remaining arguments: %s Opt needs more arguments (1 for Model; 1 more for getting, 2 more for setting)',
+                $args
+            );
+        }
     }
 }
